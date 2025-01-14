87a7db057b610332c4b9ee79e814d2d416409f4d
haniehm26 <haniehmahdavi26@gmail.com>
2025-01-14 19:55:00 +0100

    added % to cpu metric

--- virtual_piano_basic.py
+++ virtual_piano_basic.py
@@ -163,7 +163,7 @@ try:
         fps = calculate_fps(frame_count, start_time)
         cpu_usage, last_cpu_check_time = monitor_cpu_usage(last_cpu_check_time, interval=0.5)
         smoothed_cpu = smooth_cpu_usage(cpu_usage, cpu_history, max_history=10)
-        cv.putText(frame, f"fps: {fps:.2f}, cpu_usage: {smoothed_cpu:.2f}", (10, display_height - 10), cv.FONT_HERSHEY_SIMPLEX, 1.1, RED, 3, cv.LINE_AA)
+        cv.putText(frame, f"fps: {fps:.2f}, cpu_usage: {smoothed_cpu:.2f}%", (10, display_height - 10), cv.FONT_HERSHEY_SIMPLEX, 1.1, RED, 3, cv.LINE_AA)
 
         display = cv.resize(frame, (display_width, display_height)) if frame_width != display_width else frame
         cv.imshow(WINDOW_NAME, cv.addWeighted(display, 1, frame, 0.25, 1.0))
